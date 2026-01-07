bb6168127383b2d7cad94277f79c4451f620edcc
Rasya <sendiputrarasya@gmail.com>
2026-01-07 16:08:27 +0700

    Latihan 1

--- Latihan1.py
+++ Latihan1.py
@@ -1,11 +1,19 @@
+lives = 6
 secret_word = "jeruk".upper()
 secret_word1 = list(secret_word)
 count = len(secret_word1)
 guess_letter = str(input("Guess letter : ")).upper()
 while True:
-    if count > 0:
+    if count > 0 and lives > 0:
+        validGuess = False:
+        while validGuess == False:
+            guess = str(input("Guess a letter"))
+            countguess = len(guess)
+            if countguess > 1:
+                validGuess = False
+            else: 
+                validGuess = True
         
     else:
-        print("You Win")
-        test
-        
+        print("You Lose")
+        break
