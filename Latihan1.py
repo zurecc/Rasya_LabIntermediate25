b1d9b6e8c0ecc71436de64267174f7f1fe1d9d73
Rasya <sendiputrarasya@gmail.com>
2026-01-11 19:48:02 +0700

    Latihan 1

--- Latihan1.py
+++ Latihan1.py
@@ -2,20 +2,32 @@ lives = 6
 secret_word = "jeruk".upper()
 secret_word1 = list(secret_word)
 count = len(secret_word1)
-display_word = count * ("_")
+display_word = count * ["_"]
 while True:
-    if count > 0 and lives > 0:
+    if "_" in display_word and lives > 0:
         validGuess = False
-        print(display_word)
+        print("".join(display_word))
 
         while validGuess == False:
-            guess = str(input("Guess a letter : "))
+            guess = input("Guess a letter : ").upper()
             countguess = len(guess)
             if countguess > 1:
+                print("Please guess one letter at a time.")
                 validGuess = False
             else: 
                 validGuess = True
-        
-    else:
+    
+        if guess in secret_word1:
+            print(guess, "is part of the word.")
+            for num in range(len(secret_word1)):
+                if secret_word1[num] == guess:
+                    display_word[num] = guess
+        else:
+            print(guess, "is not part of the word.")
+            lives = lives - 1
+    elif lives <= 0:
         print("You Lose")
+        break
+    else:
+        print("You Win!")
         break
